4eea55c391538bc5a7f1a05c6def60efa8a140a0
TinouHD <ftb.tinouhd.yt@gmail.com>
2021-01-24 04:27:11 +0100

    Quick fix

--- src/main/java/fr/tinouhd/hqxscaler/hqx/Hqx_4x.java
+++ src/main/java/fr/tinouhd/hqxscaler/hqx/Hqx_4x.java
@@ -1,28 +1,3 @@
-/*
- * Copyright © 2003 Maxim Stepin (maxst@hiend3d.com)
- *
- * Copyright © 2010 Cameron Zemek (grom@zeminvaders.net)
- *
- * Copyright © 2011 Tamme Schichler (tamme.schichler@googlemail.com)
-
- * Copyright © 2012 A. Eduardo García (arcnorj@gmail.com)
- *
- * This file is part of hqx-java.
- *
- * hqx-java is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * hqx-java is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with hqx-java. If not, see <http://www.gnu.org/licenses/>.
- */
-
 package fr.tinouhd.hqxscaler.hqx;
 
 public class Hqx_4x extends Hqx
